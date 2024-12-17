a674a90f1be27b0a8fc6d56303fa6c6318f0689a
hnochx <hnochx@gmail.com>
2024-12-17 14:10:53 +0900

    feat: react-spring indicator answer

--- src/app/use-spring/SpringScorll.tsx
+++ src/app/use-spring/SpringScorll.tsx
@@ -1,27 +1,14 @@
 "use client";
-import { useScroll, animated, useSpring } from "@react-spring/web";
-import { useEffect, useRef, useState } from "react";
+import { useScroll, animated } from "@react-spring/web";
 
 const SpringScorll = () => {
-  const indicatorRef = useRef<HTMLDivElement>(null);
-
   const { scrollY, scrollYProgress } = useScroll();
-  const [percent, setPercent] = useState<number>(0);
-
-  const props = useSpring({
-    width: scrollYProgress.to((x) => parseFloat(x.toFixed(3)) * percent),
-  });
-
-  useEffect(() => {
-    setPercent(indicatorRef.current?.clientWidth ?? 0);
-  }, [scrollY]);
 
   return (
-    <div className="w-full h-[3000px] flex justify-center items-center relative">
+    <div className="w-full h-[2000px] flex justify-center items-center relative">
       <animated.div
-        ref={indicatorRef}
         className="fixed top-0 left-0 w-full h-12 bg-pink-400"
-        style={props}
+        style={{ width: scrollYProgress.to((v) => `${v * 100}%`) }}
       >
         <animated.span>{scrollY.to((y) => y.toFixed(0))}</animated.span>
         <br />
